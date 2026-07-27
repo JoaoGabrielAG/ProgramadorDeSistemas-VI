8755ea37b21e6907ca81815b4955b10b2e85af6b
JoaoGabrielAG <joaogabrielgarlet242@gmail.com>
2026-07-27 17:07:52 -0300

    ProgramadorDeSistemas-VI

--- 1018.c
+++ 1018.c
@@ -2,18 +2,16 @@
 
 int main ()
 {
-	int n, vet[7] = {100,50,20,10,5,2,2};
+	int n;
+	int notas[] = {100,50,20,10,5,2,1};
+	
 	scanf("%d",&n);
-	for(int i = 0; i < 1000; i++)
+	printf("%d\n",n);
+	for(int i = 0; i < 7; i++)
 	{
-		printf("%d",n);
-		n%vet[7];
-		n%vet[6];
-		n%vet[5];
-		n%vet[4];
-		n%vet[3];
-		n%vet[2];
-		n%vet[1];
+		int qnt = n / notas[i];
+		printf("%d nota(s) de R$ %d,00\n", qnt, notas[i]);
+		n = n%notas[i];
 	}
 	return(0);
 }
